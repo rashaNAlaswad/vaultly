75205876fbadda270dcaa0a305aca279e7ff197a
Rasha Alaswad <rasha.n.alaswad@gmail.com>
2026-04-04 08:55:49 +0300

    Add EmailEntryScreen with OTP submission and error handling

--- lib/features/auth/presentation/email/screens/email_entry_screen.dart
+++ lib/features/auth/presentation/email/screens/email_entry_screen.dart
@@ -1,18 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
-import '../../../../core/helpers/responsive_helper.dart';
-import '../../../../core/routing/app_routes.dart';
-import '../../../../core/themes/app_colors.dart';
+import '../../../../../core/helpers/responsive_helper.dart';
+import '../../../../../core/routing/app_routes.dart';
+import '../../../../../core/themes/app_colors.dart';
+import '../../../providers/auth_providers.dart';
 import '../../widgets/ambient_blob.dart';
 import '../widgets/email_form.dart';
 import '../widgets/hero_section.dart';
 
-class EmailEntryScreen extends StatelessWidget {
+class EmailEntryScreen extends ConsumerWidget {
   const EmailEntryScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    ref.listen<AsyncValue<void>>(sendOtpProvider, (_, state) {
+      state.whenOrNull(
+        error: (error, _) => ScaffoldMessenger.of(
+          context,
+        ).showSnackBar(SnackBar(content: Text(error.toString()))),
+      );
+    });
+
+    Future<void> onSubmit(String email) async {
+      await ref.read(sendOtpProvider.notifier).sendOtp(email);
+      final state = ref.read(sendOtpProvider);
+      if (context.mounted && !state.hasError) {
+        context.push(AppRoutes.otpVerification, extra: email);
+      }
+    }
+
+    final state = ref.watch(sendOtpProvider);
+
     return Scaffold(
       body: Stack(
         children: [
@@ -39,10 +59,8 @@ class EmailEntryScreen extends StatelessWidget {
                         const HeroSection(),
                         40.verticalSpace,
                         EmailForm(
-                          onSubmit: (email) => context.push(
-                            AppRoutes.otpVerification,
-                            extra: email,
-                          ),
+                          isLoading: state.isLoading,
+                          onSubmit: (email) async => await onSubmit(email),
                         ),
                       ],
                     ),
