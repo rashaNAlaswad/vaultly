0a3b46f349e64e7c70604aee19ec3951618f22d3
Rasha Alaswad <rasha.n.alaswad@gmail.com>
2026-04-04 17:47:39 +0300

    Add VaultIdentityAvatar widget for displaying password identity

---
+++ lib/features/add_password/presentation/widgets/vault_identity_avatar.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+import '../../../../../core/helpers/responsive_helper.dart';
+import '../../../../../core/themes/app_colors.dart';
+import '../../../../../core/themes/text_styles.dart';
+
+class VaultIdentityAvatar extends StatelessWidget {
+  const VaultIdentityAvatar({super.key, required this.letter});
+
+  final String letter;
+
+  @override
+  Widget build(BuildContext context) {
+    return Semantics(
+      label: 'Vault identity avatar: $letter',
+      child: Column(
+        children: [
+          Container(
+            width: 96,
+            height: 96,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: AppColors.primaryContainer,
+              boxShadow: const [
+                BoxShadow(color: AppColors.primaryGlow30, blurRadius: 24),
+              ],
+            ),
+            child: Center(
+              child: Text(
+                letter,
+                style: TextStyles.heroTitle.copyWith(
+                  color: AppColors.onPrimaryContainer,
+                  fontSize: 40.sp,
+                ),
+              ),
+            ),
+          ),
+          12.verticalSpace,
+          Text('VAULT IDENTITY', style: TextStyles.fieldLabel),
+        ],
+      ),
+    );
+  }
+}
