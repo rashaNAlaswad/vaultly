552331fbe590956791ee469a3d45deaa16232506
Rasha Alaswad <rasha.n.alaswad@gmail.com>
2026-04-14 10:42:07 +0300

    Refactor `LocaleNotifier` to use centralized `SharedPrefKeys` and add new preference keys

---
+++ lib/core/constants/shared_pref_keys.dart
@@ -0,0 +1,7 @@
+class SharedPrefKeys {
+  SharedPrefKeys._();
+
+  static const String locale = 'preferred_locale';
+  static const String biometricUnlock = 'settings_biometric_unlock';
+  static const String lockOnBackground = 'settings_lock_on_background';
+}
--- lib/core/providers/locale_provider.dart
+++ lib/core/providers/locale_provider.dart
@@ -2,22 +2,22 @@ import 'package:flutter/material.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-part 'locale_provider.g.dart';
+import '../constants/shared_pref_keys.dart';
 
-const _kLocaleKey = 'preferred_locale';
+part 'locale_provider.g.dart';
 
 @Riverpod(keepAlive: true)
 class LocaleNotifier extends _$LocaleNotifier {
   @override
   Future<Locale> build() async {
     final prefs = await SharedPreferences.getInstance();
-    final code = prefs.getString(_kLocaleKey) ?? 'en';
+    final code = prefs.getString(SharedPrefKeys.locale) ?? 'en';
     return Locale(code);
   }
 
   Future<void> setLocale(Locale locale) async {
     final prefs = await SharedPreferences.getInstance();
-    await prefs.setString(_kLocaleKey, locale.languageCode);
+    await prefs.setString(SharedPrefKeys.locale, locale.languageCode);
     state = AsyncData(locale);
   }
 
