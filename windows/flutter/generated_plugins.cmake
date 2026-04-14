28e0e0aa8b4f3a4438bcbd74fcc2bc7212f8e269
Rasha Alaswad <rasha.n.alaswad@gmail.com>
2026-04-14 16:29:29 +0300

    remove  sentry

--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -5,12 +5,10 @@
 list(APPEND FLUTTER_PLUGIN_LIST
   app_links
   flutter_secure_storage_windows
-  sentry_flutter
   url_launcher_windows
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
-  jni
 )
 
 set(PLUGIN_BUNDLED_LIBRARIES)
