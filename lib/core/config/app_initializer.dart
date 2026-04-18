aa37733408bb7babf1d52b59b308062cca1afd12
Rasha Alaswad <rasha.n.alaswad@gmail.com>
2026-04-18 10:28:55 +0300

    Add SharedPreferencesProvider and initialize it in AppInitializer via ProviderScope override

--- lib/core/config/app_initializer.dart
+++ lib/core/config/app_initializer.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 import '../../my_app.dart';
+import '../providers/shared_preferences_provider.dart';
 import 'flavor_config.dart';
 
 class AppInitializer {
@@ -14,6 +16,8 @@ class AppInitializer {
       flavor: isDevelopment ? Flavor.development : Flavor.production,
     );
 
+    final prefs = await SharedPreferences.getInstance();
+
     await Supabase.initialize(
       url: FlavorConfig.supabaseUrl,
       anonKey: FlavorConfig.supabasePublishableKey,
@@ -21,6 +25,13 @@ class AppInitializer {
 
     await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
 
-    runApp(const ProviderScope(child: MyApp()));
+    runApp(
+      ProviderScope(
+        overrides: [
+          sharedPreferencesProvider.overrideWithValue(prefs),
+        ],
+        child: const MyApp(),
+      ),
+    );
   }
 }
