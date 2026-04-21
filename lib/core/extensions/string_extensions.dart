78337051427035a9545e65704184cf9711f7d6c1
Rasha Alaswad <rasha.n.alaswad@gmail.com>
2026-04-21 10:29:11 +0300

    Remove app asset constants, asset paths, and OTP validation logic

--- lib/core/extensions/string_extensions.dart
+++ lib/core/extensions/string_extensions.dart
@@ -1,6 +1,4 @@
 extension StringValidation on String {
   bool get isValidEmail =>
       RegExp(r"^[a-zA-Z0-9.]+@[a-zA-Z0-9]+\.[a-zA-Z]+").hasMatch(this);
-
-  bool get isValidOtp => RegExp(r"^[0-9]{6}$").hasMatch(this);
 }
