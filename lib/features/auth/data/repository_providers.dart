8a357c5f424020b1a7b30bdf79b52c881507e244
Rasha Alaswad <rasha.n.alaswad@gmail.com>
2026-04-28 17:13:34 +0300

    Refactor repository providers to use `supabaseClientProvider` and simplify syntax

--- lib/features/auth/data/repository_providers.dart
+++ lib/features/auth/data/repository_providers.dart
@@ -1,7 +1,7 @@
 import 'package:local_auth/local_auth.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
 
+import '../../../../core/providers/supabase_provider.dart';
 import '../domain/repositories/auth_repository.dart';
 import '../domain/repositories/biometric_repository.dart';
 import 'repositories/biometric_repository_impl.dart';
@@ -10,11 +10,9 @@ import 'repositories/supabase_auth_repository.dart';
 part 'repository_providers.g.dart';
 
 @riverpod
-AuthRepository authRepository(Ref ref) {
-  return SupabaseAuthRepository(Supabase.instance.client);
-}
+AuthRepository authRepository(Ref ref) =>
+    SupabaseAuthRepository(ref.watch(supabaseClientProvider));
 
 @riverpod
-BiometricRepository biometricRepository(Ref ref) {
-  return BiometricRepositoryImpl(LocalAuthentication());
-}
+BiometricRepository biometricRepository(Ref ref) =>
+    BiometricRepositoryImpl(LocalAuthentication());
